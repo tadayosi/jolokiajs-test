814cba679617c144f80b07d9349f39dc79136154
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2023-09-18 16:30:29 +0900

    modify jquery.js

--- src/jquery.js
+++ src/jquery.js
@@ -1,12 +1,20 @@
-const jsdom = require('jsdom')
+const { JSDOM } = require('jsdom')
 const jquery = require('jquery')
 
-const dom = new jsdom.JSDOM('')
-const $ = jquery(dom.window)
+const { window } = new JSDOM('')
+const $ = jquery(window)
 
 const jolokiaUrl = 'http://localhost:10001/actuator/hawtio/jolokia'
 
+// ajax
 $.ajax(`${jolokiaUrl}/read/java.lang:type=Memory/HeapMemoryUsage/used`).done(data => {
   const response = JSON.parse(data)
-  console.log(response)
+  console.log('ajax:', response)
 })
+
+// extend
+const obj = $.extend({ a: 1 }, { b: 2 }, { c: 3 })
+console.log('extend:', obj)
+
+// each
+$.each(['a', 'b', 'c'], (i, v) => console.log('each:', i, v))
