c36d5d7ac287ab2e555d76c08cc48d4654bdbabf
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2023-09-18 18:03:07 +0900

    update basic.mjs

--- src/basic.mjs
+++ src/basic.mjs
@@ -3,10 +3,18 @@ import Jolokia from 'jolokia.js'
 const jolokiaUrl = 'http://localhost:10001/actuator/hawtio/jolokia'
 
 const jolokia = new Jolokia(jolokiaUrl)
-const response = jolokia.request({
+
+const response1 = jolokia.request({
   type: 'read',
   mbean: 'java.lang:type=Memory',
   attribute: 'HeapMemoryUsage',
   path: 'used',
 })
-console.log(response)
+console.log('read:', response1)
+
+const response2 = jolokia.request({
+  type: 'exec',
+  mbean: 'java.lang:type=Memory',
+  operation: 'gc()',
+})
+console.log('exec:', response2)
